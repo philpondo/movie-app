6707cad6f1b234bb8e1dcf0307c5bea64caf8482
phil <phil.pondo@gmail.com>
2020-06-01 19:50:50 -0400

    adds updates from params hw

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -1,6 +1,6 @@
 class Api::ActorsController < ApplicationController
  def single_actor
-   @single_actor = Actor.find_by(id: 1)
+   @single_actor = Actor.find_by(id: params['id'])
    render 'single_actor.json.jb'
  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/single_actor" => "actors#single_actor"
+    get "/single_actor/:id" => "actors#single_actor"
+    post "/single_actor" => "actors#single_actor"
     get "/all_movies" => "movies#all_movies"
     get "/single_movie" => "movies#single_movie"
   end
